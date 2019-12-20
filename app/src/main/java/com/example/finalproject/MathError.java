fc26bb9a954bb5d2aa892c6693ad4c4dbbbdae51
thumperfuzzy <thumperfuzzy@gmail.com>
2019-12-20 09:01:00 -0600

    refinished Arithmetic Error page and added app name to home screen

--- app/src/main/java/com/example/finalproject/MathError.java
+++ app/src/main/java/com/example/finalproject/MathError.java
@@ -3,9 +3,11 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MathError extends AppCompatActivity {
 
@@ -14,6 +16,11 @@ public class MathError extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_math_error);
 		Button Home = (Button)findViewById(R.id.button7);
+		ImageView imview = (ImageView) findViewById(R.id.imageView);
+		imview.setImageResource(0);
+		Drawable draw = getResources().getDrawable(R.drawable.capture);
+		draw = (draw);
+		imview.setImageDrawable(draw);
 
 		Home.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
